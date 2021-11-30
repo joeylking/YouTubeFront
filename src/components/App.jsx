482c3c377bba36470039a9d7ef7eb93329d813e8
Joey <30815220+joeylking@users.noreply.github.com>
2021-11-30 11:43:37 -0600

    App skeleton

---
+++ src/components/App.jsx
@@ -0,0 +1,9 @@
+import './App.css';
+
+const apiKey = process.env.REACT_APP_YOUTUBE_KEY;
+
+function App() {
+  return <div className='App'>App</div>;
+}
+
+export default App;
---
+++ src/components/VideoPlayer/VideoPlayer.jsx
@@ -0,0 +1,5 @@
+const VideoPlayer = () => {
+  return <div>Video</div>;
+};
+
+export default VideoPlayer;
